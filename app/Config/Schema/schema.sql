b5ea801301990bff5a343de99f4905dadde14666
Carlos Eduardo Alves <kmiksi@github>
2015-02-04 23:47:13 -0200

    correcting database schema

--- app/Config/Schema/schema.sql
+++ app/Config/Schema/schema.sql
@@ -1,6 +1,6 @@
 -- DROP TABLE posts;
 -- DROP TABLE users;
--- DROP TABLE userlevels;
+-- DROP TABLE levels;
 
 
 CREATE TABLE levels (
@@ -9,18 +9,18 @@ CREATE TABLE levels (
   description VARCHAR(45) NOT NULL DEFAULT '',
   PRIMARY KEY (id)
 );
-INSERT INTO userlevels (id, description) VALUES (1, 'admin'), (2, 'user');
+INSERT INTO levels (id, description) VALUES (1, 'admin'), (2, 'user');
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
   username VARCHAR(45) NOT NULL,
   password VARCHAR(45) NOT NULL,
-  level_id INT NOT NULL DEFAULT 2,
+  level INT NOT NULL DEFAULT 2,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
-  INDEX (level_id ASC),
-  FOREIGN KEY (level_id) REFERENCES levels (id)
+  INDEX (level ASC),
+  FOREIGN KEY (level) REFERENCES levels (id)
 );
 
 CREATE TABLE posts (
